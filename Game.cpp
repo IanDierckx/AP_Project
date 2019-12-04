deb5670d37bab9294822a6644f8a4cfa584218c2
IanDierckx <ian.dierckx@student.ua.ad.ac.be>
2019-12-04 20:30:50 +0100

    Created Stopwatch class to make sure the game runs at the same speed on every system (also already created some files for other needed classes)

--- Game.cpp
+++ Game.cpp
@@ -1,26 +1,2 @@
 #include "Game.h"
 
-#include <iostream>
-
-void Game::initializeLevel() {
-    for (int x = 0; x < grid_y; ++x) {
-        vector<pair<bool,Entity*>> row;
-        for (int y = 0; y < grid_x; ++y) {
-            row.emplace_back(false, nullptr);
-        }
-        grid.emplace_back(row);
-    }
-}
-
-void Game::printLevel() {
-    for (int x = 0; x < grid_y; ++x) {
-        for (int y = 0; y < grid_x; ++y) {
-            if (!grid[x][y].first) {
-                cout << ".";
-            } else {
-                cout << "0";
-            }
-        }
-        cout << endl;
-    }
-}
--- Game.h
+++ Game.h
@@ -2,23 +2,10 @@
 #define AP_PROJECT_GAME_H
 
 #include "Stopwatch.h"
-#include "Entity.h"
-
-#include <vector>
-
-using namespace std;
 
 //Class that combines all parts
 class Game {
-private:
-    vector<vector<pair<bool,Entity*>>> grid;
 
-    int grid_x = 9;
-    int grid_y = 7;
-public:
-    void initializeLevel();
-
-    void printLevel();
 };
 
 
