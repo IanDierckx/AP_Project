f14626e6a6e21137d57fd85c943ff258cf02ba66
IanDierckx <ian.dierckx@student.ua.ad.ac.be>
2019-12-05 12:32:45 +0100

    Added position to basic Entity class Created Game class that will combine all parts Created basic Ship class (will be used for player ship as well as enemy ships)

--- Game.cpp
+++ Game.cpp
@@ -1,2 +1,26 @@
 #include "Game.h"
 
+#include <iostream>
+
+void Game::initializeLevel() {
+    for (int x = 0; x < grid_y; ++x) {
+        vector<pair<bool,Entity*>> row;
+        for (int y = 0; y < grid_x; ++y) {
+            row.emplace_back(false, nullptr);
+        }
+        grid.emplace_back(row);
+    }
+}
+
+void Game::printLevel() {
+    for (int x = 0; x < grid_x; ++x) {
+        for (int y = 0; y < grid_y; ++y) {
+            if (!grid[x][y].first) {
+                cout << ".";
+            } else {
+                cout << "0";
+            }
+        }
+        cout << endl;
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Stopwatch.h"
+#include "Game.h"
 
 //Initialize pointer to zero so that it can be initialized in first call to getInstance
 Stopwatch *Stopwatch::instance = 0;
@@ -9,14 +10,18 @@ int main() {
     //Initialise stopwatch
     Stopwatch *watch = Stopwatch::getInstance();
 
-    int n = 0;
-    while (n<=5) {
-        if (watch->getTimePassed() == 2)  {
-            std::cout << "#" << std::endl;
-            n++;
-            watch->reset();
-        }
-    }
+//    int n = 0;
+//    while (n<=5) {
+//        if (watch->getTimePassed() == 2)  {
+//            std::cout << "#" << std::endl;
+//            n++;
+//            watch->reset();
+//        }
+//    }
+
+    Game game = Game();
+    game.initializeLevel();
+    game.printLevel();
 
     return 0;
 }
